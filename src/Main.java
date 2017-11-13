2c3e3f5a610bf8da616c5fda7a24900a550ba529
Heikki Lyhde <Heikki.Lyhde@yh.nackademin.se>
2017-11-13 13:41:29 +0100

    This is a new commit, finished with Main class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Random random = new Random();
+		int value = random.nextInt(21);
+			if (value % 2 == 0) {
+				System.out.println ("This Number is even " + value);
+			}else {
+				
+			
+				System.out.println("This number is odd " + value);
+			}
 	}
 
 }
